54d42a7c412cb7861c51aaa671f1937e7eb34726
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-04-05 09:54:18 +0200

    Update carcontroller.py

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -3,7 +3,8 @@ from common.numpy_fast import clip, interp
 from selfdrive.car import apply_toyota_steer_torque_limits, create_gas_command, make_can_msg
 from selfdrive.car.toyota.toyotacan import create_steer_command, create_ui_command, \
                                            create_ipas_steer_command, create_accel_command, \
-                                           create_acc_cancel_command, create_fcw_command
+                                           create_acc_cancel_command, create_fcw_command, create_lead_command
+
 from selfdrive.car.toyota.values import Ecu, CAR, STATIC_MSGS, SteerLimitParams
 from opendbc.can.packer import CANPacker
 import cereal.messaging as messaging
