53e0f3cc3bc2229f33003b02c238e3078147b94c
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-04-07 19:22:38 +0200

    sentry

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -19,7 +19,7 @@ if os.getenv("NOLOG") or os.getenv("NOCRASH"):
 else:
   from raven import Client
   from raven.transport.http import HTTPTransport
-  client = Client('https://1994756b5e6f41cf939a4c65de45f4f2:cefebaf3a8aa40d182609785f7189bd7@app.getsentry.com/77924',
+  client = Client('https://4b660d221577422aba9fcc337813a54a@o374486.ingest.sentry.io/5192596',
                   install_sys_hook=False, transport=HTTPTransport, release=version, tags={'dirty': dirty})
 
   def capture_exception(*args, **kwargs):
