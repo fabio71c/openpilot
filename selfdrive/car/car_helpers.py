3c39c2c361093edb9d20de9b309bb5c8dfe72288
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-05-20 12:32:24 +0200

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -217,7 +217,7 @@ def get_car(logcan, sendcan, has_relay=False):
       y = threading.Thread(target=crash_log2, args=(fingerprints,car_fw,))
       y.start()
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
+    candidate = "CAR.OLD_CAR"
 
   if not travis:
     x = threading.Thread(target=crash_log, args=(candidate,))
