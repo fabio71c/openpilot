a64976ea51c1b2a6b6a6404e00989bd68e420846
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-07-06 11:40:03 +0200

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -69,8 +69,8 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 6200.0
       
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.05]]
-      ret.lateralTuning.pid.kf = 0.00003   # full torque for 20 deg at 80mph means 0.00007818594
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.03]]
+      ret.lateralTuning.pid.kf = 0.000078   # full torque for 20 deg at 80mph means 0.00007818594
       
     elif candidate == CAR.COROLLA:
       stop_and_go = False
@@ -303,7 +303,7 @@ class CarInterface(CarInterfaceBase):
 
     if ret.enableGasInterceptor:
       ret.gasMaxBP = [0., 9., 35]
-      ret.gasMaxV = [0.05, 0.05, 0.7]
+      ret.gasMaxV = [0.05, 0.05, 0.2]
       ret.longitudinalTuning.kpV = [0.2, 0.2, 0.5]
       ret.longitudinalTuning.kiV = [0.01, 0.04]
     else:
