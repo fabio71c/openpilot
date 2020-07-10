d66fde6cae5d390bbc305a07d9017d5a5ceb1db6
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-07-10 19:01:12 +0200

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -303,7 +303,7 @@ class CarInterface(CarInterfaceBase):
 
     if ret.enableGasInterceptor:
       ret.gasMaxBP = [0., 32., 55.]
-      ret.gasMaxV = [0.05, 0.2, 0.7]
+      ret.gasMaxV = [0.05, 0.5, 0.7]
 
       ret.longitudinalTuning.kpV = [0.1, 0.2, 0.5]
       ret.longitudinalTuning.kiV = [0.01, 0.01]
