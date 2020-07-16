c08a4af222e9219eedff61960d5b59e32191d45e
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-07-16 19:37:25 +0200

    Update lane_planner.py

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -87,14 +87,15 @@ class LanePlanner():
 
     # virtual center lane, when width is big and speed is low
     if v_ego < 50. / 3.6 and current_lane_width > 4.0:
-      self.l_poly[3] += CAMERA_OFFSET + current_lane_width / 2.
+      new_l_poly = self.l_poly[3] + CAMERA_OFFSET + current_lane_width / 2.
     else:
-      self.l_poly[3] += CAMERA_OFFSET
+      new_l_poly = self.l_poly[3] + CAMERA_OFFSET
 
     # Find current lanewidth
     self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
     current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
-    self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
+    new_current_lane_width = abs(new_l_poly - self.r_poly[3])
+    self.lane_width_estimate += 0.005 * (new_current_lane_width - self.lane_width_estimate)
     speed_lane_width = interp(v_ego, [0., 31.], [2.8, 3.5])
     self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
                       (1 - self.lane_width_certainty) * speed_lane_width
