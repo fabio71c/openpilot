47703091a4210ea070c639a436575ad871065423
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-07-17 08:57:07 +0200

    Update lane_planner.py

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -83,7 +83,7 @@ class LanePlanner():
 
   def update_d_poly(self, v_ego):
     current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
-    if current_lane_width > 1.0:
+    if current_lane_width > 4.0:
       self.l_poly[3] += CAMERA_OFFSET - current_lane_width / 4
       self.r_poly[3] += CAMERA_OFFSET - current_lane_width / 4
     else:
