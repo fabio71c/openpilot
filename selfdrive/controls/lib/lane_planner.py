cc10ca24d72fefebfd037ec5201cdb722828c05f
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-07-17 19:06:22 +0000

    Update lane_planner.py

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -83,12 +83,22 @@ class LanePlanner():
 
   def update_d_poly(self, v_ego):
 
-    self.l_poly[3] += CAMERA_OFFSET
-    self.r_poly[3] += CAMERA_OFFSET
-
-    # Find current lanewidth
-    self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
     current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
+    
+    if current_lane_width > 3.7 and self.l_prob > 0.1 and self.r_prob > 0.1:
+      self.l_poly[3] += CAMERA_OFFSET - current_lane_width / 4
+      self.r_poly[3] += CAMERA_OFFSET - current_lane_width / 4
+    else:
+      self.l_poly[3] += CAMERA_OFFSET
+      self.r_poly[3] += CAMERA_OFFSET
+      
+    if current_lane_width > 3.7 and self.l_prob < 0.1 and self.r_prob > 0.1:
+      self.p_poly[3] = self.r_poly[3] + current_lane_width / 4
+    
+    if current_lane_width > 3.7 and self.l_prob < 0.1 and self.r_prob < 0.1:
+      self.p_poly[3] = self.p_poly[3] - current_lane_width / 4
+      
+    self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
     self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
     speed_lane_width = interp(v_ego, [0., 31.], [2.8, 3.5])
     self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
